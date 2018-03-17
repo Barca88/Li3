f076ceb59385c65631f5d3c7a37ae1fcf4d5edfa
Ricardo Canela <ricardocanela.ns@gmail.com>
2018-03-17 01:31:30 +0000

    Votes com informacao nessecaria

--- src/lib/parser.c
+++ src/lib/parser.c
@@ -4,7 +4,7 @@
 
 
 //Processar informacao dos users. 
-static void processNode(xmlTextReaderPtr node) {
+static void processUser(xmlTextReaderPtr node) {
     xmlChar *name;
 
     name = xmlTextReaderName(node);
@@ -21,9 +21,60 @@ static void processNode(xmlTextReaderPtr node) {
      }
 }
 
+
+//Processar informacao dos posts. 
+static void processPost(xmlTextReaderPtr node) {
+    xmlChar *name;
+
+    name = xmlTextReaderName(node);
+    if (name == NULL)
+        name = xmlStrdup(BAD_CAST "--");
+
+
+    if (xmlTextReaderHasAttributes(node)){
+        printf("------------------------------------------------------------------\n");
+         while(xmlTextReaderMoveToNextAttribute(node))
+                 printf("- %s: %s\n",
+                         xmlTextReaderName(node),
+                         xmlTextReaderValue(node));
+     }
+}
+
+//Processar informacao dos votes. 
+static void processVote(xmlTextReaderPtr node) {
+    xmlChar *name;
+    char *atributename;
+
+    name = xmlTextReaderName(node);
+    if (name == NULL)
+        name = xmlStrdup(BAD_CAST "--");
+
+
+    if (xmlTextReaderHasAttributes(node)){
+        printf("------------------------------------------------------------------\n");
+         while(xmlTextReaderMoveToNextAttribute(node)){
+             atributename = (char*)xmlTextReaderName(node); 
+             if(strcmp(atributename,"Id") == 0)
+                        printf("- %s: %s\n",
+                                 xmlTextReaderName(node),
+                                 xmlTextReaderValue(node));
+             else if(strcmp(atributename,"PostId") == 0)
+                        printf("- %s: %s\n",
+                                 xmlTextReaderName(node),
+                                 xmlTextReaderValue(node));
+
+             else if (strcmp(atributename,"VoteTypeId") == 0)
+                        printf("- %s: %s\n",
+                                 xmlTextReaderName(node),
+                                 xmlTextReaderValue(node));
+             else printf("Atribute needless-->%s\n",xmlTextReaderName(node));
+         }
+     }
+}
+
 //Montar estrutura em memoria e avancar linha a linha.
 void streamFile(char *path) {
-    char* aux = strcpy(aux,path);
+    char* aux;
     xmlTextReaderPtr stream;
     int nodeReader;
     char* dumps[3] = {"Users.xml","Posts.xml","Votes.xml"};
@@ -31,11 +82,22 @@ void streamFile(char *path) {
     for(int l = 0;l<3;l++){
          aux = strcpy(aux,path);
          stream = xmlNewTextReaderFilename(strcat(aux,dumps[l]));
-         for(int i=10;i!=0;i--)printf("%dº-------%s--------\n",i,path);
+         
          if (stream != NULL) {
              nodeReader = xmlTextReaderRead(stream);
              while (nodeReader == 1) {
-                 processNode(stream);
+                 switch(l){
+                     case 0 :    
+                         processUser(stream);
+                         break;
+                     case 1 :
+                         processPost(stream);
+                         break;
+                     case 2 :
+                         processVote(stream);
+                         break;
+                     default : printf("File unable. \n");
+                 }
                  nodeReader = xmlTextReaderRead(stream);
              }
              xmlFreeTextReader(stream);
