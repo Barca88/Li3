39b481bd96f92eeb41a293937aeea1bae526412d
Barca88 <marcomatiaspg@gmail.com>
2018-03-26 01:02:09 +0100

    posts.c e posts.h adicionados para defenir a estrutura dos posts

--- src/lib/post.c
+++ src/lib/post.c
@@ -1,6 +1,6 @@
-#include <../lib/post.h>
 #include "date.h"
 #include "post.h"
+#include <stdio.h>
 
 // Post definitions
 struct post {
@@ -12,19 +12,21 @@ struct post {
     long ownerUserID;
     char* title;
     char* tags;
-    int AnswerCount;
-    int CommentCount;
-    int FavoriteCount;
+    int answerCount;
+    int commentCount;
+    int favoriteCount;
 };
-
+//Metodo de Criação
 Post creat_post_id(long id){
     Post p = malloc(sizeof(struc post));
     p->id = id;
     p->parentID = 0;
+    p-> title = mystrdup("\0");
+    p-> tags = mystrdup("\0";
     return p;
 }
-   
-void set_PostTypeId(int postTyepeId, Post p){
+//Setters   
+void set_PostTypeId(int postTypeId, Post p){
     p->postTypeId = postTypeId;
 }
 void set_ParentID(long parentID, Post p){
@@ -40,7 +42,60 @@ void set_OwnerUserID(long ownerUserID, Post p){
     p->ownerUserID = ownerUserID;
 }
 void set_Title(char* title, Post p){
-    p->title = title;
+    free(p->title);
+    p->title = mystrdup(title);  // isto fica mesmo assim? (mystrdup)
 }
 void set_tags(char* tags, Post p){
-    p->
+    free(p->tags);
+    p->tags = mystrdup(tags);   // isto fica mesmo assim??
+}
+void set_AnswerCount(int answerCount, Post p){
+    p->answerCount = answerCount;
+}
+void set_CommentCount(int commentCount, Post p){
+    p->commentCount = commentCount;
+}
+void set_FavoriteCount(int favoriteCount, Post p){
+    p->favoriteCount = favoriteCount;
+}
+//Getters
+int get_Id(Post p){
+    return p->id;
+}
+int get_PostTypeId(Post p){
+    return p->postTypeId;
+}
+long get_ParentID(Post p){
+    return p->parentID;
+}
+Date get_CreationDate(Post p){
+    return p->creationDate;
+}
+int get_Score(Post p){
+    return p->score;
+}
+long get_OwnerUserId(Post p){
+    return p->ownerUserID;
+}
+char* get_Title(Post p){
+    return p->title;      //TODO
+} 
+char* get_Tags(Post p){
+    return p->tags;       // TODO
+}
+int get_AnswerCount(Post p){
+    return p->answerCount;
+}
+int get_CommentCount(Post p){
+    return p->commentCount;
+}
+int get_FavoriteCount(Post p){
+    return p->favoriteCount;
+}
+//Apaga o post dando free na memoria pare ele alocada
+void free_Post(Post p){
+    free(p->title);
+    free(p->tags);
+    free(p);
+}
+
