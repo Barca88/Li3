b8dc09f8b7e52d30caf7cb5a0f0af283cad5861f
Barca88 <marcomatiaspg@gmail.com>
2018-03-30 18:04:18 +0100

    date_compare

--- src/lib/date.c
+++ src/lib/date.c
@@ -20,11 +20,31 @@ int get_day(Date d) {
 }
 
 int get_month(Date d) {
-    return d->month; //change to enum? (Acho que sim. Desta forma já garantimos a limitação necessária)
+    return d->month;
 }
 
 int get_year(Date d) {
     return d->year;
+}
+
+//retorna -1 se d1 é mais antigo, 0 se iguais, 1 se d1 é mais recente
+int date_compare(Date d1, Date d2){
+    if (d1.year < d2.year)
+        return -1;
+    else if (d1.year > d2.year)
+        return 1;
+    if (d1.year == d2.year){
+        if (d1.month < d2.month)
+            return -1;
+        else if (d1.month > d2.month)
+            return 1;
+        else if (d1.day < d2.day)
+            return -1;
+        else if(d1.day > d2.day)
+            return 1;
+        else
+            return 0;
+    }
 }
 
 void free_date(Date d) {
