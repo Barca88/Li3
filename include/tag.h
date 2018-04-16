0261dc720738b5724b48083cb2925b690b124bbb
Barca88 <marcomatiaspg@gmail.com>
2018-04-16 17:37:59 +0100

    encapsulamento

--- include/tag.h
+++ include/tag.h
@@ -6,15 +6,21 @@
 typedef struct tag* ptr_tag;
 
 // Metodo de Criação
-ptr_tag init_tag(long id,char* tag, int t_ctag);
+ptr_tag create_tag(long id,char* tag, int t_ctag);
 
 //Setters
-void set_id_tag (struct tag* t, long id_out);
-void set_tag (struct tag* g, char* g_tag);
-void set_t_ctag (struct tag* c, int count_tag);
+void set_id_tag (ptr_tag t, long id_out);
+void set_tag (ptr_tag t, char* g_tag);
+void set_n_tag (ptr_tag t, int count_tag);
 
+//Getters
 long get_id_tag (ptr_tag t);
-char* get_tag (ptr_tag g);
-int get_ctag (ptr_tag c);
+char* get_tag (ptr_tag t);
+int get_n_tag (ptr_tag t);
+
+//Metodos
+int equal_tag(ptr_tag t,char* s);
+//free
+void free_tag(ptr_tag);
 
 #endif
--- src/lib/tag.c
+++ src/lib/tag.c
@@ -3,29 +3,48 @@
 struct tag {
 	long id;
 	char* tag;
-	int t_ctag;
+	int n_used;
 };
+
+ptr_tag create_tag(long id,char* s,int n){
+    ptr_tag t = malloc(sizeof(tag));
+    set_id_tag(t,id);
+    set_tag(t,s);
+    set_n_used(t,n);
+    return t;
+}
 
 long get_id_tag (ptr_tag t) {
 	return t->id;
 }
 
-char* get_tag (ptr_tag g) {
-	return g->tag;
+char* get_tag (ptr_tag t) {
+	return mystrdup(t->tag);
 }
 
-int get_ctag (ptr_tag c) {
-	return c->t_ctag;
+int get_n_used (ptr_tag t) {
+	return t->n_used;
 }
 
-void set_id_tag (struct tag* t, long id_out) {
+void set_id_tag (ptr_tag t, long id_out) {
 	t->id = id_out;
 }
 
-void set_tag (struct tag* g, char* g_tag) {
-	g->tag = g_tag;
+void set_tag (ptr_tag t, char* l) {
+    free(t->tag)
+	t->tag = mystrdup(l);
 }
 
-void set_t_ctag (struct tag* c, int count_tag) {
-	c->t_ctag = count_tag;
+void set_n_used (ptr_tag t, int count_tag) {
+	t->n_used = count_tag;
+}
+
+int equal_tag(ptr_tag t,char* s){
+    if(ptr_tag == NULL && s = '\0') return false;
+    return strcmp(t->tag,s);
+}
+
+void free_tag(ptr_tag t){
+    free(t->tag);
+    free(t);
 }
