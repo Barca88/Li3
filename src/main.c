ab5d90fa157fc7e4665bbc9f3dba75a0e5d34654
Barca88 <marcomatiaspg@gmail.com>
2018-04-24 03:46:46 +0100

    new values

--- src/main.c
+++ src/main.c
@@ -10,20 +10,20 @@ int main(int argc,char** argv){
     new = load(new,argv[1]);
 
     Date a,b;
-    a = createDate(15,7,2011);
+    a = createDate(15,7,2000);
     b = createDate(3,10,2020);
 
     info_from_post(new,1);
     top_most_active(new,12);
-    total_posts(new,a,a);
-    questions_with_tag(new,"handcent-sms",a,b);
+    total_posts(new,a,b);
+    questions_with_tag(new,"swap",a,b);
     get_user_info(new,10);
     most_voted_answers(new,11,a,b);
     most_answered_questions(new,12,a,b);
     contains_word(new,"Nexus",10);
-    both_participated(new,16575,1465,10000);
-    better_answer(new,154309);
-    most_used_best_rep(new,6,a,b);
+    both_participated(new,15811,449,10);
+    //better_answer(new,154309);
+    //most_used_best_rep(new,6,a,b);
 
     tpf =clock() - tpf;
     printf("Demorou %f segundos a ler\n",((float)tpf)/CLOCKS_PER_SEC);
