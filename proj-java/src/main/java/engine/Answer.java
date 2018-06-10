884e3a9dba9c5109fbe88125e3d697c477641c9d
Barca88 <marcomatiaspg@gmail.com>
2018-06-10 01:20:13 +0100

    Answer.java

--- proj-java/src/main/java/engine/Answer.java
+++ proj-java/src/main/java/engine/Answer.java
@@ -1,2 +1,54 @@
-public class Answer{
+public class Answer extends Post{
+    //var. de instancia
+    private long parentId; /* Id da quest à qual esta answer responde. */
+    private float average; /* variavel que define a qualidade da resposta */
+
+    public Answer(long id, LocalDate data, int score, long ownerUserId, int comment_c, long parentId, float average) {
+        super(id,data,score,ownerUserId,comment_c);        
+        this.parentId = parentId;
+        this.average = average;
+    }
+    public Answer(Answer a){
+        super(a);
+        this.parentId = g.getParentId();
+        this.average = g.getAverage();
+    }
+    //Setters
+    public void setParentId(long parentId) {
+        this.parentId = parentId;
+    }
+    public void setAverage(float average) {
+        this.average = average;
+    }
+    //Getters
+    public float getAverage() {
+        return average;
+    }
+    public long getParentId() {
+        return parentId;
+    }
+
+    //Metodos
+    public Answer clone(){
+        Answer r = new Answer(this);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Answer object = (Answer) o;
+
+        if (parentId != object.parentId) return false;
+        if (!super.equals(object)) return false;
+        return !(Float.compare(average, object.average) != 0);
+    }
+
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Answer{");
+        sb.append("parentId = ").append(getParentId());
+        sb.append(", average = ").append(getAverage());
+        return sb.append("}").toString();
+    }
+
 }
